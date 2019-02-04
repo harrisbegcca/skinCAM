07d8e49c9a88a8085b134c85d709257a19106412
Harris Beg <legostarwarsguy02@gmail.com>
2019-02-04 12:34:36 -0800

    Removed confg

--- utils/config.py
+++
@@ -1,7 +0,0 @@
-config_k = {
-	"apiKey": "AIzaSyCleQAZnEbhEW-fHl8ANOExygdL_sxxp4A",
-	"authDomain": "skincam-15b60.firebaseapp.com",
-	"databaseURL": "https://skincam-15b60.firebaseio.com",
-	"storageBucket": "skincam-15b60.appspot.com"#,
-	# "serviceAccount": r"C:\Users\legos\OneDrive\Desktop\Work\serviceAccountKey.json"
-}
